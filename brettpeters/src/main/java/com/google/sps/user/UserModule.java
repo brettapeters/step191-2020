fb332195e1a11b003cb8d2fbd103dd3da117382d
Brett Peters <brettpeters@google.com>
2020-06-26 07:11:28 +0000

    Read name from URL params

--- brettpeters/src/main/java/com/google/sps/user/UserModule.java
+++ brettpeters/src/main/java/com/google/sps/user/UserModule.java
@@ -2,16 +2,23 @@ package com.google.sps.user;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.name.Names;
+import com.google.inject.name.Named;
+import com.google.inject.servlet.RequestParameters;
+import com.google.inject.servlet.RequestScoped;
+import java.util.Map;
 
 /**
  * Guice module for injecting user information
  */
 public class UserModule extends AbstractModule {
 
-  @Override
-  protected void configure() {
-    bindConstant().annotatedWith(Names.named("username")).to("Brett");
+  @Provides
+  @Named("username")
+  @RequestScoped
+  public String provideUserName(@RequestParameters Map<String, String[]> params) {
+    if (params.containsKey("name")) {
+      return params.get("name")[0];
+    }
+    return "World";
   }
 }
